6ee74e8a61539f44c6d0540668ddb58f002dbcdb
holifieldw <holifield04@gmail.com>
2025-10-30 01:19:50 +0800

    login page ui

--- chat-client/src/pages/LoginPage.tsx
+++ chat-client/src/pages/LoginPage.tsx
@@ -1,5 +1,29 @@
+import { Button, TextInput } from "flowbite-react";
+import { Card } from "flowbite-react/components/Card";
+import { useNavigate } from "react-router-dom";
+
 function LoginPage() {
-  return <h1>Login Page</h1>;
+  const navigate = useNavigate();
+
+  function handleLogin(e: React.FormEvent) {
+    e.preventDefault();
+    navigate("/home");
+  }
+
+  return (
+    <div className="size-full bg-sky-200 text-white flex items-center justify-center">
+      <Card className="text-center max-w-md w-md p-4 drop-shadow-lg drop-shadow-slate-400">
+        <h1 className="text-3xl">WebChat</h1>
+        <p className="text-start text-sm">
+          Provide username to access chat room
+        </p>
+        <form onSubmit={handleLogin} className="flex flex-col gap-4">
+          <TextInput id="username" placeholder="Username" required shadow/>
+          <Button type="submit">Enter chatroom</Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
 export default LoginPage;
