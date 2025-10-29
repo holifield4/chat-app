48e93105dea57bfef8f5fc4616dd774a8b10d93c
holifieldw <holifield04@gmail.com>
2025-10-30 01:18:39 +0800

    wrap pages with page layout

--- chat-client/src/App.tsx
+++ chat-client/src/App.tsx
@@ -1,20 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import { lazy, Suspense } from "react";
 import Loading from "./components/loading/Loading";
+import PageLayout from "./layout/PageLayout";
 
 const LoginPage = lazy(() => import("./pages/LoginPage"));
+const Home = lazy(() => import("./pages/Home"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loading/>}>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Suspense>
+      <PageLayout>
+        <Suspense fallback={<Loading />}>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </Suspense>
+      </PageLayout>
     </BrowserRouter>
   );
 }
