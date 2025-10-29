0a6cb216df1dd9a38da3ccd25ca05574a651e656
holifieldw <holifield04@gmail.com>
2025-10-30 01:16:49 +0800

    add flowbite to vite config

--- chat-client/vite.config.ts
+++ chat-client/vite.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import flowbiteReact from "flowbite-react/plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
     tailwindcss(),
+    flowbiteReact(),
   ],
   server: {
-    port: 4000
-  }
-})
+    port: 4000,
+  },
+});
