438b38deb6c2fcd99cd5a1956715ea2fab774ed1
holifieldw <holifield04@gmail.com>
2025-10-30 15:53:53 +0800

    modify styling

--- chat-client/src/pages/Home.tsx
+++ chat-client/src/pages/Home.tsx
@@ -3,11 +3,11 @@ import ChatRoom from "../components/chat/ChatRoom";
 
 function Home() {
   return (
-    <div className="size-full flex">
-      <div className="w-2/6 border-r border-slate-500/30">
+    <div className="h-screen flex bg-gradient-to-br from-slate-50 to-slate-100">
+      <div className="w-2/6 border-r border-slate-200/60 bg-white/80 backdrop-blur-sm">
         <ChatLobby/>
       </div>
-      <div className="w-4/6">
+      <div className="flex-1">
         <ChatRoom />
       </div>
     </div>
