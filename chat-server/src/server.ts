4ca5d098c0a6920ea9d80e15583b2bc8208a04a2
holifieldw <holifield04@gmail.com>
2025-10-31 00:31:34 +0800

    update imports

--- chat-server/src/server.ts
+++ chat-server/src/server.ts
@@ -1,5 +1,5 @@
-import { server } from "./socket/socket.ts";
+import { server } from './app';
 
 server.listen(3000, () => {
-  console.log("🚀🚀🚀 Server is running on port 3000")
-})
+  console.log('🚀🚀🚀 Server is running on port 3000');
+});
