7f242eaef7e5826954a85379c5cc0d7373e519ef
holifieldw <holifield04@gmail.com>
2025-10-30 15:53:04 +0800

    disabled flowbite dark mode

--- chat-client/src/App.tsx
+++ chat-client/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loading from "./components/loading/Loading";
 import PageLayout from "./layout/PageLayout";
+import { ThemeInit } from "../.flowbite-react/init";
 
 const LoginPage = lazy(() => import("./pages/LoginPage"));
 const Home = lazy(() => import("./pages/Home"));
@@ -9,6 +10,8 @@ const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
   return (
+    <>
+    <ThemeInit/>
     <BrowserRouter>
       <PageLayout>
         <Suspense fallback={<Loading />}>
@@ -20,6 +23,7 @@ function App() {
         </Suspense>
       </PageLayout>
     </BrowserRouter>
+    </>
   );
 }
 
