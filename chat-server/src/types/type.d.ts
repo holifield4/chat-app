28aa3529e4f20212e0a06bc4b0378c0a85cf5fcf
holifieldw <holifield04@gmail.com>
2025-10-31 00:30:25 +0800

    additional types for socket instances

--- chat-server/src/types/type.d.ts
+++ chat-server/src/types/type.d.ts
@@ -1,6 +1,36 @@
+import { Server, Socket } from 'socket.io';
+
 export interface Message {
-    id: string;
-    text: string;
-    username: string;
-    timestamp: number;
+  id: string;
+  text: string;
+  username: string;
+  timestamp: number;
 }
+
+interface ServerToClientEvents {
+  noArg: () => void;
+  basicEmit: (a: number, b: string, c: Buffer) => void;
+  withAck: (d: string, callback: (e: number) => void) => void;
+}
+
+interface ClientToServerEvents {
+  setUsername: (username) => void;
+  chats: (message) => void;
+  getRooms: (callback: (rooms: Room[]) => void) => void;
+}
+
+interface SocketData {
+  username: string;
+}
+
+export interface Room {
+  name: string;
+  userCount: number;
+}
+
+export type IO = Server<ClientToServerEvents, ServerToClientEvents, SocketData>;
+export type AppSocket = Socket<
+  ClientToServerEvents,
+  ServerToClientEvents,
+  SocketData
+>;
