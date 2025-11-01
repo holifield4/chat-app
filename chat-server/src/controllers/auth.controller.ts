9e91c98a1ba8d30e88ce004cc24402dadbd3a51a
holifieldw <holifield04@gmail.com>
2025-11-01 23:25:32 +0800

    implement jwt on login

--- chat-server/src/controllers/auth.controller.ts
+++ chat-server/src/controllers/auth.controller.ts
@@ -1,20 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 import { SocketData } from '../types/type';
-import { io } from '../app';
+import jwt from 'jsonwebtoken';
+import config from '../config/config';
 
 /**
- * Simple authroization implementation
+ * Simple authentication implementation
  */
 
 export const login = (req: Request, res: Response, next: NextFunction) => {
   try {
     const { username } = req.body as SocketData;
 
-    if (!username) {
+    if (!username || username.trim() === '') {
       res.status(401).json({ error: 'Please provide username' });
     }
 
-    res.status(200).json({ message: 'Successfully login' });
+    const token = jwt.sign({ username }, config.jwtSecret, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ message: 'Successfully login', token });
   } catch (error) {
     next(error);
   }
