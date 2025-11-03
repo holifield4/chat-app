96b120a2893a58e1c6fed9e8edf45dd69c4d41fd
holifieldw <holifield04@gmail.com>
2025-11-03 17:20:55 +0800

    wip channel handler implementation

--- chat-server/src/handlers/channel.handler.ts
+++ chat-server/src/handlers/channel.handler.ts
@@ -1,36 +1,40 @@
 import { AppSocket, IO, Room } from '../types/type';
 
-export const channelHandler = (io: IO, socket: AppSocket) => {
-  const getRooms = (cb: (rooms: Room[]) => void) => {
+//helper to update room detail when user join/leave
+export const updateRoom = (io: IO) => {
+  getRoomList(io, (rooms) => {
+    io.emit("rooms", rooms)
+  })
+}
 
+//function to get room list and no. of user in room
+const getRoomList = (io: IO, cb: (rooms: Room[]) => void) => {
+  const allExistingRooms = io.sockets.adapter.rooms;
+  const allConnectedUsers = io.sockets.sockets;
+  const roomList: Room[] = [];
+
+  //eliminates rooms that by default named as the socket id itself
+  for (const [roomId, connectedSocketIds] of allExistingRooms.entries()) {
+    if (!allConnectedUsers.has(roomId)) {
+      const room: Room = {
+        name: roomId,
+        userCount: connectedSocketIds.size,
+      };
+
+      roomList.push(room);
+    }
+  }
+  //send the room list to the client
+  cb(roomList);
+};
+
+export const channelHandler = (io: IO, socket: AppSocket) => {
+  socket.on('getRoomList', (cb: (rooms: Room[]) => void) => {
     if (typeof cb !== 'function') {
-      console.warn(
-        "Client emitted 'getRooms' without an acknowledgement callback. Skipping response.",
-      );
+      //return if not ack
       return;
     }
+    getRoomList(io, cb);
+  });
 
-    const allRooms = io.sockets.adapter.rooms;
-    const connectedUser = io.sockets.sockets;
-
-    const roomsList: Room[] = [];
-
-    for (const [roomId, socketIds] of allRooms.entries()) {
-      if (!connectedUser.has(roomId)) {
-        const room: Room = {
-          name: roomId,
-          userCount: socketIds.size,
-        };
-
-        roomsList.push(room);
-      }
-    }
-
-    console.log(
-      `Sending ${roomsList.length} public rooms list to ${socket.data.username || socket.id}`,
-    );
-    cb(roomsList);
-  };
-
-  socket.on('getRooms', getRooms);
 };
