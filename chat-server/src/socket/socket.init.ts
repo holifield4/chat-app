b6592a50ba2d721cce888e5d1ed138a50eee78f4
holifieldw <holifield04@gmail.com>
2025-11-03 17:21:21 +0800

    update socket init

--- chat-server/src/socket/socket.init.ts
+++ chat-server/src/socket/socket.init.ts
@@ -2,20 +2,20 @@ import { authHandler } from '../handlers/auth.handler';
 import { channelHandler } from '../handlers/channel.handler';
 import { AppSocket, IO } from '../types/type';
 import { verifyHandshake } from '../middleware/auth.middleware';
+import { messageHandler } from '../handlers/message.handler';
 
 /**
  *
  *  Events handler
  */
 export const initEventHandlers = (io: IO) => {
+  //verify handshake connecting
   verifyHandshake(io);
 
   io.on('connection', (socket: AppSocket) => {
-    authHandler(socket);
+    authHandler(io, socket);
     channelHandler(io, socket);
+    messageHandler(io, socket);
 
-    socket.on('chats', (message) => {
-      console.log(`[${socket.data.username}]: ${message}`);
-    });
   });
 };
