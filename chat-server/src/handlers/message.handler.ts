b5efc3b2e5ab14d594d90d3c917bec41f6976f87
holifieldw <holifield04@gmail.com>
2025-11-03 17:21:02 +0800

    wip message handler implementation

---
+++ chat-server/src/handlers/message.handler.ts
@@ -0,0 +1,21 @@
+import { randomUUID } from 'crypto';
+import type { AppSocket, IO, Message, MessageContent } from '../types/type';
+import { getCurrentTime } from '../utils/time.util';
+
+export const messageHandler = (io: IO, socket: AppSocket) => {
+  
+  const sendMessage = (msgContent: MessageContent) => {
+    const payload: Message = {
+        id: randomUUID(),
+        roomId: msgContent.toRoom,
+        username: socket.data.username,
+        message: msgContent.messageContent,
+        timestamp: getCurrentTime()
+    };
+
+    //broadcast the message
+    io.to(payload.roomId).emit('message', payload);
+  };
+
+  socket.on('sendMessage', sendMessage);
+};
