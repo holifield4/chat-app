61ebf8203a4d7630d07310150ed5514f731a9cf1
holifieldw <holifield04@gmail.com>
2025-11-03 17:20:45 +0800

    wip auth handler

--- chat-server/src/handlers/auth.handler.ts
+++ chat-server/src/handlers/auth.handler.ts
@@ -1,17 +1,25 @@
 import type { AppSocket, IO } from '../types/type';
+import { updateRoom } from './channel.handler';
 
 export const authHandler = (io: IO, socket: AppSocket) => {
-    
-  //associated username to socket after successfully logged in
-  const setUsername = (payload: string) => {
-    console.log(`@${payload} logged in with socket id: ${socket.id}`);
-    socket.data.username = payload;
-  };
+  //join the client to general room by default on connected
+  socket.join('general');
+  //update room count when client join
+  updateRoom(io);
+  //broadcast to the general room when user joined
+  io
+    .to('general')
+    .emit('userJoined', { username: socket.data.username, room: 'general' });
 
-  const disconnect = () => {
-    console.log(`@${socket.data.username}[${socket.id}] has disconnected`);
-  };
+  socket.on('disconnecting', () => {
+    //update room for everyone when a user is about to disconnect
+    //this is called before the socket leaves the rooms
+    setTimeout(() => updateRoom(io), 0);
+  });
 
-  socket.on('setUsername', setUsername);
-  socket.on('disconnect', disconnect);
+  socket.on('disconnect', () => {
+    console.log(`@${socket.data.username} [${socket.id}] has disconnected`);
+    //update room user count
+    updateRoom(io);
+  });
 };
