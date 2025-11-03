e795af0f1953b9c743789c45e712623d87f16438
holifieldw <holifield04@gmail.com>
2025-11-03 17:05:47 +0800

    modify auth middleware

--- chat-server/src/middleware/auth.middleware.ts
+++ chat-server/src/middleware/auth.middleware.ts
@@ -1,19 +1,17 @@
-import { AppSocket, IO } from '../types/type';
+import type { AppSocket, IO, SocketData } from '../types/type';
 import jwt from 'jsonwebtoken';
 import config from '../config/config';
 
 export const verifyHandshake = (io: IO) => {
   io.use((socket: AppSocket, next: (err?: Error) => void) => {
-    const token = socket.handshake.auth.token;
+    const token = socket.handshake.auth.token as string | null;
 
     if (!token) {
       return next(new Error('Authentication error: Token not provided'));
     }
 
     try {
-      const decoded = jwt.verify(token, config.jwtSecret) as {
-        username: string;
-      };
+      const decoded = jwt.verify(token, config.jwtSecret) as SocketData;
       socket.data.username = decoded.username;
       next();
     } catch (err) {
