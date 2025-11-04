b63ecdaba11274afa24084db0e1bc76cb4bee2d0
holifieldw <holifield04@gmail.com>
2025-11-04 23:00:12 +0800

    add new icon

---
+++ chat-client/src/assets/icons/Plus.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from "react";
+
+export function Plus(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      {/* Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE */}
+      <path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z" />
+    </svg>
+  );
+}
