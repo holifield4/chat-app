e080d78c91152d98b11b17725d980c0cb9766dab
holifieldw <holifield04@gmail.com>
2025-11-05 23:09:04 +0800

    implement new actions and states

--- chat-client/src/store/useRoom.ts
+++ chat-client/src/store/useRoom.ts
@@ -4,9 +4,18 @@ import { appSocket } from "./useSocket";
 
 const useRoom = create<RoomStore>((set) => ({
   roomList: [],
+  availableMembers: [],
   setRoomList: (newRoomList) => set({ roomList: newRoomList }),
   createRoom: (newRoom) => {
     appSocket.emit("createRoom", newRoom);
+  },
+  inviteMembers: (members) => {
+    appSocket.emit('inviteMembers', members);
+  },
+  getAvailableMembers: (roomName) => {
+    appSocket.emit('getAvailableMembers', roomName, (members)=>{
+      set({ availableMembers: members })
+    })
   }
 }));
 
