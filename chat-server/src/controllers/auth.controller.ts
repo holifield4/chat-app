5198afb69eae6ce115a42d04b15f85acbf0d9127
holifieldw <holifield04@gmail.com>
2025-11-06 18:36:53 +0800

    add middleware to check if username is exist

--- chat-server/src/controllers/auth.controller.ts
+++ chat-server/src/controllers/auth.controller.ts
@@ -2,17 +2,28 @@ import { NextFunction, Request, Response } from 'express';
 import { SocketData } from '../types/type';
 import jwt from 'jsonwebtoken';
 import config from '../config/config';
+import { checkUsername } from '../middleware/auth.middleware';
 
 /**
  * Simple authentication implementation
  */
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   try {
     const { username } = req.body as SocketData;
 
     if (!username || username.trim() === '') {
       res.status(401).json({ error: 'Please provide username' });
+    }
+
+    const isTaken = await checkUsername(username.trim());
+
+    if (isTaken) {
+      res.status(409).json({ error: 'Username is already taken' });
     }
 
     const token = jwt.sign({ username }, config.jwtSecret, {
