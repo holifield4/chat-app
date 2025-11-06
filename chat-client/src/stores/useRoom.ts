88a8629cf7b6f910a6453513f601bf429c8fd9da
holifieldw <holifield04@gmail.com>
2025-11-06 18:35:58 +0800

    add indicator message when creating new room

--- chat-client/src/stores/useRoom.ts
+++ chat-client/src/stores/useRoom.ts
@@ -1,22 +1,29 @@
 import { create } from "zustand";
 import type { RoomStore } from "../types/rooms.types";
 import { appSocket } from "./useSocket";
+import { toast } from "../utils/toast";
 
 const useRoom = create<RoomStore>((set) => ({
   roomList: [],
   availableMembers: [],
   setRoomList: (newRoomList) => set({ roomList: newRoomList }),
   createRoom: (newRoom) => {
-    appSocket.emit("createRoom", newRoom);
+    appSocket.emit("createRoom", newRoom, (success, message) => {
+      if (success) {
+        toast(message, "success");
+      } else {
+        toast(message, "error");
+      }
+    });
   },
   inviteMembers: (members) => {
-    appSocket.emit('inviteMembers', members);
+    appSocket.emit("inviteMembers", members);
   },
   getAvailableMembers: (roomName) => {
-    appSocket.emit('getAvailableMembers', roomName, (members)=>{
-      set({ availableMembers: members })
-    })
-  }
+    appSocket.emit("getAvailableMembers", roomName, (members) => {
+      set({ availableMembers: members });
+    });
+  },
 }));
 
 export default useRoom;
